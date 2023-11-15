e68faa7eca5f9a7c44f351b2a28d2dcb47517a4e
Daskendrik <ss599@yandex.ru>
2023-11-15 18:40:14 +0300

    -/-

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const http = require('http');
 
-http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+  });
+  res.end('<h1>Hello Node</h1>');
+});
+
+server.listen(3000, () => {
+  console.log('Server start');
+});

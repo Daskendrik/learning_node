1715a2b7ba0857ca2594eea9d3685ecd67f4f752
Daskendrik <ss599@yandex.ru>
2023-11-17 19:45:21 +0300

    add html

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/html',
-  });
-  res.end('<h1>Hello Node</h1>');
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, {
+//     'Content-Type': 'text/html',
+//   });
+//   res.end('<h1>Hello Node</h1>');
+// });
+
+// server.listen(3000, () => {
+//   console.log('Server start');
+// });
+
+import express from 'express';
+import path from 'path';
+
+const __dirname = path.resolve();
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'static', 'index.html'));
 });
 
-server.listen(3000, () => {
-  console.log('Server start');
-});
+app.listen(PORT, () => {
+  console.log(`Server start, port ${PORT}...`);
+}); //запускаем серве
